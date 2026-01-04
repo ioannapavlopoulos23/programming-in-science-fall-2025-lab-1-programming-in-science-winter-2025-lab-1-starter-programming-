3b7676ced3a26e543dca77637168e194a2b8484c
ioannapavlopoulos23 <ioannapavlopoulos2313@gmail.com>
2026-01-04 16:54:17 -0500

    Update Lab1.py
    Lab1 correct version

--- Lab1.py
+++ Lab1.py
@@ -1,12 +1,16 @@
 # Function 1: Write a simple Hello World program
 # This function should print "Hello, World!" to the screen.
 def hello_world():
-    # TODO: Implement this function
-    pass  # Replace with your code
+    print("Hello, World!")
 
 # Function 2: Get input and output with different variable types
 # This function should prompt the user for their name (string), age (int), and height (float),
 # and then print them back in a formatted message.
 def input_output():
-    # TODO: Implement this function
-    pass  # Replace with your code
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    height = float(input("Enter your height:"))
+    print("Hello," + " " + name + "!")
+    print("You are" + " " + str(age) + " " + "years old." )
+    print("Your height is" + " " + str(height) + " " + "meters.")
+
